420d04e6456b5ec01c91fd4f2ccb65614190c6e7
Henry Nguyen <nguyenhai1591@gmail.com>
2020-04-27 20:07:44 +1000

    Update

--- forof.js
+++ forof.js
@@ -10,4 +10,10 @@ for (let moiBuaAn in buaAn) console.log(moiBuaAn)
 for (let moiChuCai in bangChuCai) console.log(moiChuCai)
 
 // for (let moiBuaAn of buaAn) console.log(moiBuaAn)
+for (let moiBuaAn in buaAn) console.log(buaAn[moiBuaAn])
 for (let moiChuCai of bangChuCai) console.log(moiChuCai)
+
+// bien ten = '...'
+// Loop tung ki tu trong bien ten va in ki tu do viet hoa
+ten = 'henry'
+for (let kiTu of ten) console.log(kiTu.toUpperCase())
