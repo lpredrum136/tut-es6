973f4e55c73f4bb8a906a0599f123d2d6d2dd001
Henry Nguyen <nguyenhai1591@gmail.com>
2020-04-27 20:27:35 +1000

    Update

--- forof.js
+++ forof.js
@@ -6,14 +6,14 @@ const buaAn = {
 
 const bangChuCai = ['a', 'b', 'c']
 
-for (let moiBuaAn in buaAn) console.log(moiBuaAn)
-for (let moiChuCai in bangChuCai) console.log(moiChuCai)
+// for (let moiBuaAn in buaAn) console.log(moiBuaAn)
+// for (let moiChuCai in bangChuCai) console.log(moiChuCai)
 
-// for (let moiBuaAn of buaAn) console.log(moiBuaAn)
-for (let moiBuaAn in buaAn) console.log(buaAn[moiBuaAn])
-for (let moiChuCai of bangChuCai) console.log(moiChuCai)
+// for (let moiChuCai of bangChuCai) console.log(moiChuCai)
+
+// for (let moiBuaAn in buaAn) console.log(buaAn[moiBuaAn])
 
 // bien ten = '...'
 // Loop tung ki tu trong bien ten va in ki tu do viet hoa
-ten = 'henry'
+const ten = 'henry'
 for (let kiTu of ten) console.log(kiTu.toUpperCase())
